c5e2e1ad11c3c6eaf0186dc1b7b704da9a2123ac
17325763383zyw-star <17325763383zyw@gmail.com>
2026-04-28 21:09:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-window.SNAKE_SUPABASE_URL = window.SNAKE_SUPABASE_URL || '';
-window.SNAKE_SUPABASE_ANON_KEY = window.SNAKE_SUPABASE_ANON_KEY || '';
+window.SNAKE_SUPABASE_URL = 'https://hnytshfuyurcejzblwik.supabase.co';
+window.SNAKE_SUPABASE_ANON_KEY = 'sb_publishable_DuZmEtZKqIJfvdFlKUivDA_3w0evMyb';
